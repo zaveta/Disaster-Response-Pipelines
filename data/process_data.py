77592fd248ebe5dd173bc2ecd7778f6d94da7ba1
zaveta <54998579+zaveta@users.noreply.github.com>
2020-11-09 22:26:55 +0000

    process_data.py update

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,60 @@
+# import libraries
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    function to read data
+    input:
+    - messages_filepath
+    - categories_filepath
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return messages, categories
+    df = messages.merge(categories)
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    data cleaning function
+    input:
+    - merged df
+    output:
+    - cleaned df
+    '''
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(';', expand=True)
+    # select the first row of the categories dataframe
+    row = categories[categories.index == 0]
+
+    # use this row to extract a list of new column names for categories.
+    category_colnames = pd.Series(row.values[0]).apply(lambda x: x[:-2]).tolist()
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = pd.Series(categories[column].values).apply(lambda x: x[-1:])
+        # convert column from string to numeric
+        categories[column] = categories[column].astype('int64')
+    # drop the original categories column from `df`
+    df = df.drop("categories", axis=1)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    function for saving df to database
+    input:
+    - df
+    - database_filename
+    '''
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('disaster_table', engine, index=False, if_exists='replace')
 
 
 def main():
@@ -24,12 +68,11 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+ 
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-        
+        save_data(df, database_filepath)     
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
